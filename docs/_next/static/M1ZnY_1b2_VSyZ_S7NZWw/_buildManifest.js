d24f940056a3718ddcecda9b3cfcf422ef4b38ad
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-02-21 18:10:50 +0700

    add docs

---
+++ docs/_next/static/M1ZnY_1b2_VSyZ_S7NZWw/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/fbb05736a869ed81.js"
+  ],
+  "/404": [
+    "static/chunks/b8793cd163b0c4fd.js"
+  ],
+  "/500": [
+    "static/chunks/d620099e9523f497.js"
+  ],
+  "/_error": [
+    "static/chunks/d754deb3533e6302.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/500",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/chunks/b8793cd163b0c4fd.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/404", [
+  "static/chunks/1bb547923b902cfc.js",
+  "static/chunks/6d7f11ceea69566c.js",
+  "static/chunks/87a7c5bed3c57248.js",
+  "static/chunks/turbopack-179410ffa30cfb6e.js"
+])
